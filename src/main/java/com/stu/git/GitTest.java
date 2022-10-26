5fed61e8e6f603fe6521385aa43f91cb5ddd12a9
LaiLai <LaiLai@hwj.com>
2022-10-26 09:32:45 +0800

    first commit

---
+++ src/main/java/com/stu/git/GitTest.java
@@ -0,0 +1,10 @@
+package com.stu.git;/*
+@author lailai
+@create 2022-10-26-9:30
+*/
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("我不是赖赖");
+    }
+}
